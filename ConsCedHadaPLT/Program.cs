fb3c6718df264672772cc336170af74ef87d7409
Alberto Martínez Quesada <betomqcr@gmail.com>
2024-08-26 11:43:07 -0600

    Configuracion del proyecto para trabajar en inicio

---
+++ ConsCedHadaPLT/Program.cs
@@ -0,0 +1,18 @@
+using ConsCedHadaPLT;
+
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+startup.Configure(app, app.Environment);
+
+app.Run();
